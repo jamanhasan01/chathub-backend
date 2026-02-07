74b46872c7e3d8dc5df1657920a4bee4ab43bba1
jaman hasan <jamanhasan246@gmail.com>
2026-02-08 04:04:36 +0600

    update

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -8,7 +8,7 @@ const server = createServer(app)
 const io = new Server(server, {
   transports: ['websocket'],
   cors: {
-    origin: 'https://chathub-frontend-beta.vercel.app',
+       origin: ['https://chathub-frontend-beta.vercel.app','http://localhost:5173'],
     methods: ['GET', 'POST'],
   },
 })
