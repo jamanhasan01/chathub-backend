619057076bb5325df1e2887ed37ae6448efc604f
jaman hasan <jamanhasan246@gmail.com>
2026-02-08 06:01:29 +0600

    jwt update

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,14 @@
-import { NextFunction } from 'express'
-import jwt, { Secret } from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import { JWTPayload } from '../types/auth.type'
 
 export const generateToken = (payload: JWTPayload) => {
   const secret = process.env.JSON_TOKEN_SECRET
+
   if (!secret) {
     throw new Error('JWT secret is not found')
   }
-  return jwt.sign(payload, secret as Secret, { expiresIn: '1h' })
+
+  return jwt.sign(payload, secret, {
+    expiresIn: '1h',
+  })
 }
